ad070a693adfcf3daca4c4566d9c5e7984a4ad23
Ignacio Rehbein <78046088+Nach0t@users.noreply.github.com>
2024-03-06 10:06:29 -0300

    Clase 1

---
+++ Matematicas discretas/Codigo 1.py
@@ -0,0 +1,9 @@
+def calcular_año(año):
+    if (año % 4 == 0 and año % 100 != 0) or (año % 400 == 0):
+        print("Es bisiesto")
+    else:
+        print("No es bisiesto")
+
+año = int(input("Ingrese un año: "))
+
+print(calcular_año(año))
